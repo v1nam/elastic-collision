933706d8bf2d5d1cd1c403ad40f5d7c057772530
saiTama-max <vinamraj01@gmail.com>
2021-06-28 14:48:03 +0530

    Elastic collision for circles with sweep and prune

---
+++ src/main.rs
@@ -0,0 +1,161 @@
+use macroquad::prelude::*;
+use macroquad::rand::{gen_range, ChooseRandom};
+
+#[derive(Copy, Clone, PartialEq, Debug)]
+struct Particle {
+    position: Vec2,
+    velocity: Vec2,
+    color: Color,
+    radius: f32,
+    id: usize,
+}
+
+impl Particle {
+    fn new(radius: f32, color: Color, id: usize) -> Self {
+        Particle {
+            position: vec2(
+                gen_range(0., screen_width()),
+                gen_range(0., screen_height()),
+            ),
+            velocity: vec2(
+                gen_range(2.0, 4.0) * vec![-1., 1.].choose().unwrap(),
+                gen_range(2.0, 4.0) * vec![-1., 1.].choose().unwrap(),
+            ),
+            color,
+            radius,
+            id,
+        }
+    }
+    fn update(&mut self) {
+        self.velocity = self.velocity.clamp_length_max(5.6);
+        self.position += self.velocity;
+        if self.position.x + self.radius >= screen_width() {
+            self.position.x = screen_width() - self.radius;
+        } else if self.position.x - self.radius <= 0. {
+            self.position.x = 0. + self.radius;
+        }
+        if self.position.y + self.radius >= screen_height() {
+            self.position.y = screen_height() - self.radius;
+        } else if self.position.y - self.radius <= 0. {
+            self.position.y = 0. + self.radius;
+        }
+        if self.position.x + self.radius == screen_width() || self.position.x - self.radius == 0. {
+            self.velocity.x *= -1.;
+        }
+        if self.position.y + self.radius == screen_height() || self.position.y - self.radius == 0. {
+            self.velocity.y *= -1.;
+        }
+    }
+}
+
+#[macroquad::main("Collision")]
+async fn main() {
+    let mut particles: Vec<Particle> = Vec::new();
+    let colors = vec![
+        Color::from_rgba(129, 161, 193, 255),
+        Color::from_rgba(191, 97, 106, 255),
+        Color::from_rgba(208, 135, 112, 255),
+        Color::from_rgba(163, 190, 140, 255),
+        Color::from_rgba(235, 203, 139, 255),
+        Color::from_rgba(143, 188, 187, 255),
+        Color::from_rgba(136, 192, 208, 255),
+    ];
+    for id in 0..15 {
+        particles.push(Particle::new(
+            gen_range(10.0, 20.0),
+            *colors.choose().unwrap(),
+            id,
+        ));
+    }
+    loop {
+        clear_background(Color::from_rgba(36, 42, 54, 255));
+        particles.sort_by(|a, b| {
+            vec2(a.position.x + a.radius, a.position.y + a.radius)
+                .x
+                .partial_cmp(&vec2(b.position.x + b.radius, b.position.y + b.radius).x)
+                .unwrap()
+        });
+        let mut active_list: Vec<Particle> = vec![particles[0]];
+        let mut possible_collisions: Vec<(usize, usize)> = Vec::new();
+        for (pind, particle) in particles.iter().enumerate() {
+            let mut particles_to_remove: Vec<usize> = Vec::new();
+            let mut particles_to_add: Vec<Particle> = Vec::new();
+            for active_particle in active_list.iter() {
+                if active_particle.id != particle.id {
+                    if particle.position.x - particle.radius
+                        > active_particle.position.x + active_particle.radius
+                    {
+                        particles_to_remove.push(active_particle.id);
+                    } else {
+                        possible_collisions.push((pind, active_particle.id));
+                    }
+                    particles_to_add.push(*particle);
+                }
+            }
+            for ptr in particles_to_remove {
+                active_list.retain(|p| p.id != ptr);
+            }
+            for pta in particles_to_add {
+                active_list.push(pta);
+            }
+        }
+
+        for (p_pos, ap_id) in possible_collisions {
+            let ap_pos = particles.iter().position(|&p| p.id == ap_id).unwrap();
+            let part = particles[p_pos];
+            let actpart = particles[ap_pos];
+
+            let dist = part.position.distance(actpart.position);
+            if dist <= part.radius + actpart.radius {
+                let ndist = (part.position - actpart.position).normalize();
+                let touch_distance = dist * (actpart.radius / (actpart.radius + part.radius));
+                let contact = actpart.position + (ndist * touch_distance);
+
+                particles[ap_pos].position = contact - (ndist * actpart.radius);
+                particles[p_pos].position = contact + (ndist * part.radius);
+
+                let p_vel = part.velocity.length();
+                let ap_vel = actpart.velocity.length();
+                let p_dir = part.velocity.y.atan2(part.velocity.x);
+                let ap_dir = actpart.velocity.y.atan2(actpart.velocity.x);
+
+                let contact_dir = ndist.y.atan2(ndist.x);
+                let p_mass = part.radius.powf(3.);
+                let ap_mass = actpart.radius.powf(3.);
+
+                let t = (ap_vel * (ap_dir - contact_dir).cos() * (ap_mass - p_mass)
+                    + 2. * p_mass * p_vel * (p_dir - contact_dir).cos())
+                    / (ap_mass + p_mass);
+                particles[ap_pos].velocity.x = t * contact_dir.cos()
+                    + ap_vel
+                        * (ap_dir - contact_dir).sin()
+                        * (contact_dir + std::f32::consts::PI / 2.).cos();
+                particles[ap_pos].velocity.y = t * contact_dir.sin()
+                    + ap_vel
+                        * (ap_dir - contact_dir).sin()
+                        * (contact_dir + std::f32::consts::PI / 2.).sin();
+                let t = (p_vel * (p_dir - contact_dir).cos() * (p_mass - ap_mass)
+                    + 2. * ap_mass * ap_vel * (ap_dir - contact_dir).cos())
+                    / (ap_mass + p_mass);
+                particles[p_pos].velocity.x = t * -(contact_dir.cos())
+                    + p_vel
+                        * (p_dir - contact_dir).sin()
+                        * -(contact_dir + std::f32::consts::PI / 2.).cos();
+                particles[p_pos].velocity.y = t * -(contact_dir.sin())
+                    + p_vel
+                        * (p_dir - contact_dir).sin()
+                        * -(contact_dir + std::f32::consts::PI / 2.).sin();
+            }
+        }
+        for particle in particles.iter_mut() {
+            particle.update();
+            draw_circle(
+                particle.position.x,
+                particle.position.y,
+                particle.radius,
+                particle.color,
+            );
+        }
+        next_frame().await;
+    }
+}
